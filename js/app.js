4a037b435a52c09d8dab1210f2df91e7187246a0
arindamad <32026868+arindamad@users.noreply.github.com>
2019-09-24 14:40:52 +0530

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
-imgs = "";
+    imgs = "";
         $(".arImagesAnch").on('click', function(e){
             var imgs = $(this).html();
             $("#myModal").modal("show");
             $('.modal-body').html(imgs);
         });
-        $('#myModal [data-dismiss="modal"]').on('click', function(){            
+        // $('#myModal [data-dismiss="modal"], #myModal').on('click', function(){            
+            
+        // });
+        $('#myModal').on('hidden.bs.modal', function (e) {
             val = window.location.href.split('#')[0];
             function ChangeUrl(page, url) {
                 if (typeof (history.pushState) != "undefined") {
@@ -20,9 +23,10 @@ imgs = "";
         $(document).ready(function(){
             var myHash = window.location.hash;
             if(myHash!=""){                
-                console.log(myHash);
+                // console.log(myHash);
                 $("#myModal").modal("show");
                 var images = $('a[href="'+myHash+'"]').html();
                 $('.modal-body').html(images);
             }           
         });
+       
