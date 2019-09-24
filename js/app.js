ee392f291100513c40013472369a429847e37648
arindamad <32026868+arindamad@users.noreply.github.com>
2019-09-24 13:42:03 +0530

    added html css javascript

---
+++ js/app.js
@@ -0,0 +1,28 @@
+imgs = "";
+        $(".arImagesAnch").on('click', function(e){
+            var imgs = $(this).html();
+            $("#myModal").modal("show");
+            $('.modal-body').html(imgs);
+        });
+        $('#myModal [data-dismiss="modal"]').on('click', function(){            
+            val = window.location.href.split('#')[0];
+            function ChangeUrl(page, url) {
+                if (typeof (history.pushState) != "undefined") {
+                    var obj = {Page: page, Url: url};
+                    history.pushState(obj, obj.Page, obj.Url);
+                } else {
+                    window.location.href = "homePage";                    
+                }
+            }
+            ChangeUrl('Page1', val);
+        });
+
+        $(document).ready(function(){
+            var myHash = window.location.hash;
+            if(myHash!=""){                
+                console.log(myHash);
+                $("#myModal").modal("show");
+                var images = $('a[href="'+myHash+'"]').html();
+                $('.modal-body').html(images);
+            }           
+        });
